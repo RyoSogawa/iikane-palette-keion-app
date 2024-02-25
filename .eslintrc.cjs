7e4eaf637db84088843f6e9121c188e0669381c0
RyoSogawa <koashimitekara.1122@gmail.com>
2024-02-25 21:55:26 +0900

    chore:setting eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,17 @@ const config = {
   parserOptions: {
     project: true,
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint', 'jsx-a11y', 'react', 'react-hooks', 'unused-imports'],
   extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'prettier',
   ],
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
@@ -31,6 +37,77 @@ const config = {
         checksVoidReturn: { attributes: false },
       },
     ],
+
+    'arrow-body-style': 'off',
+    'no-alert': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': 2,
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-sort-props': [
+      'error',
+      {
+        shorthandLast: true,
+        callbacksLast: true,
+        multiline: 'ignore',
+        ignoreCase: false,
+        noSortAlphabetically: true,
+        reservedFirst: ['key', 'ref'],
+      },
+    ],
+    'react/function-component-definition': 'off',
+    'react/require-default-props': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/space-before-blocks': 'off',
+    'object-shorthand': ['error', 'properties'],
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    'no-restricted-exports': 'off',
+    'consistent-return': 'off',
+    'import/extensions': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'object',
+          'index',
+          'type',
+        ],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        pathGroups: [
+          {
+            pattern: '{react,react-dom}',
+            group: 'builtin',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin', 'object'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        labelComponents: [],
+        labelAttributes: [],
+        controlComponents: [],
+        assert: 'either',
+        depth: 25,
+      },
+    ],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'unused-imports/no-unused-imports': 'error',
   },
 };
 
--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,17 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
