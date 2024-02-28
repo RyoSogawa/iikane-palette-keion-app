ac97d2ca31740d66135f12c0c2e71de94bd41e90
RyoSogawa <koashimitekara.1122@gmail.com>
2024-02-28 09:50:40 +0900

    feat:ControlledRichEditor component

--- src/components/model/UserForm/index.tsx
+++ src/components/model/UserForm/index.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { Button, Stack } from '@mantine/core';
 
 import { type UserFormValues, useUserForm } from '@/components/model/UserForm/logics';
+import ControlledRichEditor from '@/components/ui/ControlledRichEditor';
 import ControlledTextInput from '@/components/ui/ControlledTextInput';
 
 export type UserFormProps = {
@@ -17,8 +18,9 @@ const UserForm: React.FC<UserFormProps> = ({ user }) => {
   return (
     <form onSubmit={handleSubmit}>
       <Stack>
-        <ControlledTextInput label="お名前" control={control} name="name" />
+        <ControlledTextInput label="お名前" control={control} name="name" required />
         <ControlledTextInput label="ニックネーム" control={control} name="nickname" />
+        <ControlledRichEditor label="自己紹介" control={control} name="introduction" />
       </Stack>
       <Button type="submit" variant="outline" mt={32} size="lg">
         保存する
---
+++ src/components/ui/ControlledRichEditor/index.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import '@mantine/tiptap/styles.css';
+import React from 'react';
+
+import { Input, type InputWrapperProps } from '@mantine/core';
+import { Link as LinkExtension, RichTextEditor } from '@mantine/tiptap';
+import { useEditor } from '@tiptap/react';
+import { StarterKit } from '@tiptap/starter-kit';
+import { type FieldValues, useController, type UseControllerProps } from 'react-hook-form';
+
+export type ControlledRichEditorProps<T extends FieldValues> = Omit<
+  InputWrapperProps,
+  'error' | 'children'
+> &
+  Pick<UseControllerProps<T>, 'name' | 'control' | 'disabled' | 'rules'>;
+
+const ControlledRichEditor = <T extends FieldValues>({
+  name,
+  control,
+  disabled,
+  rules,
+  ...props
+}: ControlledRichEditorProps<T>) => {
+  const {
+    field: { onBlur, ref, value, onChange },
+    fieldState: { error },
+  } = useController({
+    name,
+    control,
+    disabled,
+    rules,
+  });
+
+  const editor = useEditor({
+    extensions: [StarterKit, LinkExtension],
+    content: value,
+    editable: !disabled,
+    onBlur,
+    onUpdate: (arg) => {
+      onChange(arg.editor.getHTML());
+    },
+  });
+
+  return (
+    <Input.Wrapper {...props} error={error?.message}>
+      <RichTextEditor editor={editor}>
+        <RichTextEditor.Content ref={ref} />
+      </RichTextEditor>
+    </Input.Wrapper>
+  );
+};
+
+export default ControlledRichEditor;
