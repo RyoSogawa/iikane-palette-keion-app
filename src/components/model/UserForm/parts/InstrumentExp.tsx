37914bdf6cdb69058b2fe0bbf8f08321faa5f8b4
RyoSogawa <koashimitekara.1122@gmail.com>
2024-03-02 23:16:35 +0900

    feat:remove user part

--- src/components/model/UserForm/parts/InstrumentExp.tsx
+++ src/components/model/UserForm/parts/InstrumentExp.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 
-import { Box, Button, Group, Stack } from '@mantine/core';
+import { Box, Button, Group, type SelectProps, Stack } from '@mantine/core';
 import { type Control, useFieldArray } from 'react-hook-form';
 
 import { type UserFormValues } from '@/components/model/UserForm/logics';
@@ -8,7 +8,7 @@ import ControlledSelect from '@/components/ui/ControlledSelect';
 import ControlledTextInput from '@/components/ui/ControlledTextInput';
 import { UserPartMap } from '@/constants/user-part';
 
-const partIcons = Object.entries(UserPartMap).map(([value, label]) => ({
+const partIcons: SelectProps['data'] = Object.entries(UserPartMap).map(([value, label]) => ({
   value,
   label: label.name,
 }));
@@ -18,7 +18,7 @@ export type InstrumentExpProps = {
 };
 
 const InstrumentExp: React.FC<InstrumentExpProps> = ({ control }) => {
-  const { fields, append } = useFieldArray({
+  const { fields, append, remove } = useFieldArray({
     control,
     name: 'UserParts',
   });
@@ -32,6 +32,13 @@ const InstrumentExp: React.FC<InstrumentExpProps> = ({ control }) => {
     });
   }, [append, fields.length]);
 
+  const handleClickRemove = useCallback(
+    (index: number) => () => {
+      remove(index);
+    },
+    [remove],
+  );
+
   return (
     <Stack>
       {fields.map((field, index) => (
@@ -40,13 +47,17 @@ const InstrumentExp: React.FC<InstrumentExpProps> = ({ control }) => {
             control={control}
             name={`UserParts.${index}.partIcon`}
             data={partIcons}
+            w={130}
           />
           <ControlledTextInput
             control={control}
             name={`UserParts.${index}.remark`}
-            placeholder="パートの詳細や経験年数等について教えてね"
+            placeholder="パート詳細や経験年数等について教えてね"
             flex="1"
           />
+          <Button variant="subtle" color="red" onClick={handleClickRemove(index)}>
+            -
+          </Button>
         </Group>
       ))}
       <Box>
