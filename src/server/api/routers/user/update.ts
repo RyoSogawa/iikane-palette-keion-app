00d825abd0769f56bde37c6b50bbdf37f6098ccc
RyoSogawa <koashimitekara.1122@gmail.com>
2024-03-02 22:23:13 +0900

    feat:update UserPart table

--- src/server/api/routers/user/update.ts
+++ src/server/api/routers/user/update.ts
@@ -1,14 +1,25 @@
-import { type z } from 'zod';
+import { z } from 'zod';
 
 import { protectedProcedure } from '@/server/api/trpc';
-import { UserSchema } from '@/types/generated/zod';
+import { UserPartSchema, UserSchema } from '@/types/generated/zod';
 
-const userUpdateProfileInputSchema = UserSchema.pick({
-  id: true,
-  name: true,
-  nickname: true,
-  introduction: true,
-});
+const userUpdateProfileInputSchema = z.intersection(
+  UserSchema.pick({
+    id: true,
+    name: true,
+    nickname: true,
+    introduction: true,
+  }),
+  z.object({
+    UserPart: z.array(
+      UserPartSchema.pick({
+        partIcon: true,
+        order: true,
+        remark: true,
+      }),
+    ),
+  }),
+);
 
 export type UserUpdateProfileInput = z.infer<typeof userUpdateProfileInputSchema>;
 
@@ -19,19 +30,34 @@ export const updateProfile = protectedProcedure
       throw new Error('Invalid user id');
     }
 
-    return ctx.db.user.upsert({
-      where: {
-        id: ctx.session.user.id,
-      },
-      create: {
-        name: input.name,
-        nickname: input.nickname,
-        introduction: input.introduction,
-      },
-      update: {
-        name: input.name,
-        nickname: input.nickname,
-        introduction: input.introduction,
-      },
+    return ctx.db.$transaction(async (tx) => {
+      const user = await tx.user.upsert({
+        where: {
+          id: ctx.session.user.id,
+        },
+        create: {
+          name: input.name,
+          nickname: input.nickname,
+          introduction: input.introduction,
+        },
+        update: {
+          name: input.name,
+          nickname: input.nickname,
+          introduction: input.introduction,
+        },
+      });
+
+      await tx.userPart.deleteMany({
+        where: {
+          userId: user.id,
+        },
+      });
+
+      await tx.userPart.createMany({
+        data: input.UserPart.map((part) => ({
+          ...part,
+          userId: user.id,
+        })),
+      });
     });
   });
