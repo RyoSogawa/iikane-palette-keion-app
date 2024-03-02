c74ba225846baa030a0012f88c7249666847fc35
RyoSogawa <koashimitekara.1122@gmail.com>
2024-03-02 22:51:29 +0900

    feat:user part map

---
+++ src/constants/user-part.ts
@@ -0,0 +1,50 @@
+import { type PartIconType } from '@/types/generated/zod';
+
+export const UserPartMap = {
+  VOCAL: {
+    icon: '🎤',
+    name: 'ボーカル',
+  },
+  RAP: {
+    icon: '🎤',
+    name: 'ラップ',
+  },
+  GUITAR: {
+    icon: '🎸',
+    name: 'ギター',
+  },
+  BASS: {
+    icon: '🎸',
+    name: 'ベース',
+  },
+  DRUM: {
+    icon: '🥁',
+    name: 'ドラム',
+  },
+  KEYBOARD: {
+    icon: '🎹',
+    name: 'キーボード',
+  },
+  WIND: {
+    icon: '🎷',
+    name: '管楽器',
+  },
+  STRINGS: {
+    icon: '🎻',
+    name: '弦楽器',
+  },
+  PERCUSSION: {
+    icon: '🥁',
+    name: '打楽器',
+  },
+  OTHERS: {
+    icon: '🎵',
+    name: 'その他',
+  },
+} as const satisfies Record<
+  PartIconType,
+  {
+    name: string;
+    icon: string;
+  }
+>;
