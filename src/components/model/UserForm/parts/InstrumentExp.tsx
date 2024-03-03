bd93b019d0b6d21e65784d9ca2e3123502e45e0c
RyoSogawa <koashimitekara.1122@gmail.com>
2024-03-03 17:48:07 +0900

    refactor:separate DnDContext component

--- src/components/model/UserForm/parts/InstrumentExp.tsx
+++ src/components/model/UserForm/parts/InstrumentExp.tsx
@@ -1,23 +1,12 @@
 import React, { useCallback } from 'react';
 
-import {
-  DndContext,
-  closestCenter,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors,
-  type DragEndEvent,
-} from '@dnd-kit/core';
-import {
-  SortableContext,
-  sortableKeyboardCoordinates,
-  verticalListSortingStrategy,
-} from '@dnd-kit/sortable';
+import { type DragEndEvent } from '@dnd-kit/core';
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import { Box, Button, Space, Stack } from '@mantine/core';
 import { IconPlus } from '@tabler/icons-react';
 import { type Control, useFieldArray } from 'react-hook-form';
 
+import DnDContext from '@/components/functional/DnDContext';
 import { type UserFormValues } from '@/components/model/UserForm/logics';
 import InstrumentExpItem from '@/components/model/UserForm/parts/InstrumentExpItem';
 
@@ -40,30 +29,22 @@ const InstrumentExp: React.FC<InstrumentExpProps> = ({ control }) => {
     });
   }, [append, fields.length]);
 
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    }),
-  );
-
   const handleDragEnd = useCallback(
     (event: DragEndEvent) => {
       const { active, over } = event;
 
       if (!!over && active.id !== over.id) {
-        move(
-          Number(active.data.current?.sortable.index),
-          Number(over.data.current?.sortable.index),
-        );
+        const fromIndex = fields.findIndex((field) => field.id === active.id);
+        const toIndex = fields.findIndex((field) => field.id === over.id);
+        move(fromIndex, toIndex);
       }
     },
-    [move],
+    [fields, move],
   );
 
   return (
     <Stack>
-      <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <DnDContext onDragEnd={handleDragEnd}>
         <SortableContext items={fields} strategy={verticalListSortingStrategy}>
           {fields.map((field, index) => (
             <InstrumentExpItem
@@ -75,7 +56,7 @@ const InstrumentExp: React.FC<InstrumentExpProps> = ({ control }) => {
             />
           ))}
         </SortableContext>
-      </DndContext>
+      </DnDContext>
       <Box>
         <Button variant="outline" color="gray" onClick={handleClickAppend}>
           <IconPlus size={16} />
