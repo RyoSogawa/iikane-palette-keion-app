d7d3fec2967704a291d18576e6bbdc781a103858
RyoSogawa <koashimitekara.1122@gmail.com>
2024-03-03 17:55:18 +0900

    feat:change cursor during dragging

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@mantine/core/styles.css';
 import '@mantine/tiptap/styles.css';
 import '@mantine/notifications/styles.css';
+import '../styles/global.css';
 
 import React from 'react';
 
--- src/components/functional/DnDContext/index.tsx
+++ src/components/functional/DnDContext/index.tsx
@@ -1,4 +1,4 @@
-import React, { type ComponentProps } from 'react';
+import React, { type ComponentProps, useCallback } from 'react';
 
 import {
   DndContext as DnDKitContext,
@@ -7,12 +7,17 @@ import {
   PointerSensor,
   useSensor,
   useSensors,
+  type DragEndEvent,
+  type DragStartEvent,
 } from '@dnd-kit/core';
 import { sortableKeyboardCoordinates } from '@dnd-kit/sortable';
 
-export type DnDContextProps = ComponentProps<typeof DnDKitContext>;
+export type DnDContextProps = Omit<
+  ComponentProps<typeof DnDKitContext>,
+  'sensors' | 'collisionDetection'
+>;
 
-const DnDContext: React.FC<DnDContextProps> = ({ children, ...props }) => {
+const DnDContext: React.FC<DnDContextProps> = ({ children, onDragStart, onDragEnd, ...props }) => {
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -20,8 +25,30 @@ const DnDContext: React.FC<DnDContextProps> = ({ children, ...props }) => {
     }),
   );
 
+  const handleDragStart = useCallback(
+    (event: DragStartEvent) => {
+      onDragStart?.(event);
+      document.body.classList.add('dragging');
+    },
+    [onDragStart],
+  );
+
+  const handleDragEnd = useCallback(
+    (event: DragEndEvent) => {
+      onDragEnd?.(event);
+      document.body.classList.remove('dragging');
+    },
+    [onDragEnd],
+  );
+
   return (
-    <DnDKitContext sensors={sensors} collisionDetection={closestCenter} {...props}>
+    <DnDKitContext
+      {...props}
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+    >
       {children}
     </DnDKitContext>
   );
