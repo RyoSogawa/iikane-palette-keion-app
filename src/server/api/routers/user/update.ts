d8b0fdac3e012217f1246c7388ef9ec8284498f3
RyoSogawa <koashimitekara.1122@gmail.com>
2024-03-14 22:21:12 +0900

    feat:update UserTag

--- src/server/api/routers/user/update.ts
+++ src/server/api/routers/user/update.ts
@@ -1,20 +1,25 @@
-import { type z } from 'zod';
+import { z } from 'zod';
 
 import { protectedProcedure } from '@/server/api/trpc';
 import { UserSchema } from '@/types/generated/zod';
 
-const userUpdateProfileInputSchema = UserSchema.pick({
-  id: true,
-  name: true,
-  nickname: true,
-  residence: true,
-  introduction: true,
-  instagramLink: true,
-  twitterLink: true,
-  musicLink: true,
-  podcastLink: true,
-  websiteLink: true,
-});
+const userUpdateProfileInputSchema = z.intersection(
+  UserSchema.pick({
+    id: true,
+    name: true,
+    nickname: true,
+    residence: true,
+    introduction: true,
+    instagramLink: true,
+    twitterLink: true,
+    musicLink: true,
+    podcastLink: true,
+    websiteLink: true,
+  }),
+  z.object({
+    tags: z.array(z.string()),
+  }),
+);
 
 export type UserUpdateProfileInput = z.infer<typeof userUpdateProfileInputSchema>;
 
@@ -25,20 +30,55 @@ export const updateProfile = protectedProcedure
       throw new Error('Invalid user id');
     }
 
-    return ctx.db.user.update({
-      where: {
-        id: ctx.session.user.id,
-      },
-      data: {
-        name: input.name,
-        nickname: input.nickname,
-        residence: input.residence,
-        introduction: input.introduction,
-        instagramLink: input.instagramLink,
-        twitterLink: input.twitterLink,
-        musicLink: input.musicLink,
-        podcastLink: input.podcastLink,
-        websiteLink: input.websiteLink,
-      },
+    return ctx.db.$transaction(async (tx) => {
+      await tx.userTag.createMany({
+        data: input.tags.map((tag) => ({
+          name: tag,
+        })),
+        skipDuplicates: true,
+      });
+
+      const userTags = await tx.userTag.findMany({
+        where: {
+          name: {
+            in: input.tags,
+          },
+        },
+      });
+
+      await tx.userOnUserTag.deleteMany({
+        where: {
+          NOT: {
+            userTagId: {
+              in: userTags.map((tag) => tag.id),
+            },
+          },
+        },
+      });
+
+      await tx.userOnUserTag.createMany({
+        data: userTags.map((tag) => ({
+          userTagId: tag.id,
+          userId: ctx.session.user.id,
+        })),
+        skipDuplicates: true,
+      });
+
+      return tx.user.update({
+        where: {
+          id: ctx.session.user.id,
+        },
+        data: {
+          name: input.name,
+          nickname: input.nickname,
+          residence: input.residence,
+          introduction: input.introduction,
+          instagramLink: input.instagramLink,
+          twitterLink: input.twitterLink,
+          musicLink: input.musicLink,
+          podcastLink: input.podcastLink,
+          websiteLink: input.websiteLink,
+        },
+      });
     });
   });
