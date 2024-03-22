25602a6836f1eb54799e31e4a5ac368e00cf1fc0
RyoSogawa <koashimitekara.1122@gmail.com>
2024-03-22 16:12:01 +0900

    feat:remove storage when deleting account

--- src/components/model/AccountDeleteButton/actions.ts
+++ src/components/model/AccountDeleteButton/actions.ts
@@ -1,10 +1,24 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
+import { cookies } from 'next/headers';
 
 import { api } from '@/trpc/server';
+import { createSupabaseServerClient } from '@/utils/supabase';
 
 export const deleteAccount = async (userId: string) => {
+  const cookieStore = cookies();
+  const supabase = createSupabaseServerClient(cookieStore);
+
+  const { data, error } = await supabase.storage.from('profiles').list(userId);
+  if (error) {
+    throw error;
+  }
+  const filePaths = data?.map((file) => `${userId}/${file.name}`);
+  if (filePaths) {
+    await supabase.storage.from('profiles').remove(filePaths);
+  }
+
   await api.user.deleteMe.mutate();
   revalidatePath('/');
   revalidatePath(`/members/${userId}`);
