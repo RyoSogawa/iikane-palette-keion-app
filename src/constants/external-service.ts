cda1c99d606b62b9d175b9a9058c2fa44ec69f07
RyoSogawa <koashimitekara.1122@gmail.com>
2024-03-26 00:05:07 +0900

    refactor:use typed env

--- src/constants/external-service.ts
+++ src/constants/external-service.ts
@@ -1,4 +1,6 @@
-export const IIKANE_GUILD_ID = process.env.NEXT_PUBLIC_IIKANE_GUILD_ID!;
+import { env } from '@/env';
+
+export const IIKANE_GUILD_ID = env.NEXT_PUBLIC_IIKANE_GUILD_ID;
 
 export const LINK = {
   IIKANE_PALETTE: 'https://palette.jp.net/',
--- src/constants/supabase.ts
+++ src/constants/supabase.ts
@@ -1,1 +1,3 @@
-export const STORAGE_PATH = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public`;
+import { env } from '@/env';
+
+export const STORAGE_PATH = `${env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public`;
