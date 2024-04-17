a0ec95a9d3b23077d3257a5f5dc1b60026e197d9
RyoSogawa <koashimitekara.1122@gmail.com>
2024-04-17 10:44:19 +0900

    feat:add leftSlot prop and ref forwarding

--- src/features/my-best-songs/components/MusicCard/index.tsx
+++ src/features/my-best-songs/components/MusicCard/index.tsx
@@ -5,32 +5,39 @@ import { Box, Group, Image, Paper, Text } from '@mantine/core';
 import MusicTypeBadge from '@/features/my-best-songs/components/MusicTypeBadge';
 import { type SongTypeType } from '@/types/generated/zod';
 
-export type MusicCardProps = {
+export type MusicCardProps = React.ComponentProps<'div'> & {
   type: SongTypeType;
   artist: string;
   name: string;
   image?: string;
+  leftSlot?: React.ReactElement;
   rightSlot?: React.ReactElement;
 };
 
-const MusicCard: React.FC<MusicCardProps> = ({ artist, image, name, type, rightSlot }) => {
-  return (
-    <Paper p="xs" bg="transparent">
-      <Group h="100%" wrap="nowrap">
-        <Image src={image} alt={name} w={60} width={60} height={60} loading="lazy" bg="gray" />
-        <Box flex={1} pb={4}>
-          <MusicTypeBadge type={type} />
-          <Text mt={4} size="sm" fw={500} c="bright" lineClamp={1}>
-            {name}
-          </Text>
-          <Text lineClamp={1} c="dimmed" fz="xs">
-            {artist}
-          </Text>
-        </Box>
-        {rightSlot}
-      </Group>
-    </Paper>
-  );
-};
+const MusicCard = React.forwardRef<HTMLDivElement, MusicCardProps>(
+  ({ artist, image, name, type, leftSlot, rightSlot, ...props }, forwardedRef) => {
+    return (
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      <Paper ref={forwardedRef} p="xs" bg="transparent" {...props}>
+        <Group h="100%" wrap="nowrap">
+          {leftSlot}
+          <Image src={image} alt={name} w={60} width={60} height={60} loading="lazy" bg="gray" />
+          <Box flex={1} pb={4}>
+            <MusicTypeBadge type={type} />
+            <Text mt={4} size="sm" fw={500} c="bright" lineClamp={1}>
+              {name}
+            </Text>
+            <Text lineClamp={1} c="dimmed" fz="xs">
+              {artist}
+            </Text>
+          </Box>
+          {rightSlot}
+        </Group>
+      </Paper>
+    );
+  },
+);
 
+MusicCard.displayName = 'MusicCard';
 export default MusicCard;
