eb230115993d8cf30e2ee771809a5b2a0b412b52
RyoSogawa <koashimitekara.1122@gmail.com>
2024-04-26 23:17:57 +0900

    feat:add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,6 @@ import Providers from '@/components/functional/Providers';
 export const metadata: Metadata = {
   title: 'いいかねパレット軽音部',
   description: 'いいかねパレット軽音部のポータルサイトです。',
-  icons: [{ rel: 'icon', url: '/favicon.ico' }],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -22,6 +21,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ja">
       <head>
         <ColorSchemeScript defaultColorScheme="dark" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#242424" />
       </head>
       <body>
         <Providers>
