65286f24a55da024f5b3acc6c1a906c9809e11d7
RyoSogawa <koashimitekara.1122@gmail.com>
2024-04-26 09:27:59 +0900

    feat:improve MusicTypeBadge props

--- src/features/my-best-songs/components/MusicTypeBadge/index.tsx
+++ src/features/my-best-songs/components/MusicTypeBadge/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
-import { Badge } from '@mantine/core';
+import { Badge, type MantineSize } from '@mantine/core';
 
 import { type SongTypeType } from '@/types/generated/zod';
 
 export type MusicTypeBadgeProps = {
   type: SongTypeType;
+  size?: MantineSize;
 };
 
-const MusicTypeBadge: React.FC<MusicTypeBadgeProps> = ({ type }) => {
+const MusicTypeBadge: React.FC<MusicTypeBadgeProps> = ({ type, size = 'xs' }) => {
   return (
-    <Badge variant="light" color={type === 'track' ? 'indigo' : 'pink'} size="xs">
+    <Badge variant="light" color={type === 'track' ? 'indigo' : 'pink'} size={size}>
       {type}
     </Badge>
   );
