74465c5f0c5565454ae0462479949ecd8849d90d
RyoSogawa <koashimitekara.1122@gmail.com>
2024-04-27 16:11:33 +0900

    feat:test ogp

---
+++ src/app/members/[userId]/(view)/opengraph-image.tsx
@@ -0,0 +1,40 @@
+import { ImageResponse } from 'next/og';
+
+import { SITE_NAME } from '@/constants/site-info';
+
+export const config = {
+  runtime: 'edge',
+};
+
+export const alt = SITE_NAME;
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = 'image/png';
+
+export default async function Image() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'black',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        👋 Hello
+      </div>
+    ),
+    {
+      ...size,
+    },
+  );
+}
