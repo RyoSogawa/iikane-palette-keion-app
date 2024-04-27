b52f56029afcfef039def2887081f1269aed6687
RyoSogawa <koashimitekara.1122@gmail.com>
2024-04-27 22:38:05 +0900

    Revert "refactor:use icon api"
    This reverts commit 281726bc07bfda31088b72da9c14d6684951ce01.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ja">
       <head>
         <ColorSchemeScript defaultColorScheme="dark" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#242424" />
       </head>
       <body>
         <Providers>
