789beb9681e5909243139e965f0fbd938708ae7f
RyoSogawa <koashimitekara.1122@gmail.com>
2024-04-27 23:38:59 +0900

    fix:use process.env to set metadataBase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     default: SITE_NAME,
   },
   description: 'いいかねパレット軽音部のポータルサイトです。',
-  metadataBase: new URL('https://iikane-palette-keion-app.vercel.app/'),
+  metadataBase: new URL(process.env.NEXTAUTH_URL!), // HACK: env.NEXTAUTH_URLだと正常に取得できないのでprocessから指定
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
