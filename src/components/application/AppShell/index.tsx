55fcaca60b23910532c13b11e1ce68097fc459e1
RyoSogawa <koashimitekara.1122@gmail.com>
2024-07-23 23:08:53 +0900

    feat:NavMenu component

--- src/components/application/AppShell/index.tsx
+++ src/components/application/AppShell/index.tsx
@@ -10,10 +10,12 @@ import {
   Space,
   Title,
   Divider,
+  Text,
 } from '@mantine/core';
 import Link from 'next/link';
 
 import Footer from '@/components/application/Footer';
+import NavMenu from '@/components/application/NavMenu';
 import AccountMenuButton from '@/features/account/components/AccountMenuButton';
 
 import s from './style.module.css';
@@ -29,12 +31,14 @@ const AppShell: React.FC<AppShellProps> = ({ children }) => {
     <MantineAppShell header={{ height: HEADER_HEIGHT }} padding="md">
       <MantineAppShell.Header>
         <Container px={20} h="100%">
-          <Flex align="center" h="100%">
+          <Flex align="center" h="100%" gap={8}>
             <Box component={Link} href="/" td="none">
               <Title order={1} size="1rem" c="bright">
                 いいかねパレット軽音部
               </Title>
             </Box>
+            <Text c="dimmed">/</Text>
+            <NavMenu />
             <Space flex={1} />
             <AccountMenuButton />
           </Flex>
---
+++ src/components/application/NavMenu/index.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import React, { useMemo } from 'react';
+
+import { ActionIcon, Menu } from '@mantine/core';
+import { IconCaretDownFilled, IconHome, IconUsersGroup } from '@tabler/icons-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+type MenuItem = {
+  title: string;
+  icon: React.ReactElement;
+  pathname: string;
+};
+
+const MenuMap: Record<string, MenuItem> = {
+  home: { title: 'Home', icon: <IconHome size={14} />, pathname: '/' },
+  users: { title: '部員名簿', icon: <IconUsersGroup size={14} />, pathname: '/members' },
+};
+
+const NavMenu: React.FC = () => {
+  const currentPath = usePathname();
+
+  const currentMenu = useMemo(() => {
+    return Object.entries(MenuMap)
+      .slice()
+      .sort(([, a], [, b]) => {
+        // パターンマッチをなるべく厳格にするために長いpathnameから優先的にfindする
+        return b.pathname.length - a.pathname.length;
+      })
+      .find(([pageTitle, { pathname }]) => {
+        // homeはパスがシンプルなので完全一致で判定
+        if (pageTitle === 'home') {
+          return currentPath === pathname;
+        }
+        // その他のページはサブディレクトリも含めて判定
+        return currentPath.startsWith(pathname);
+      })?.[1];
+  }, [currentPath]);
+
+  return (
+    <Menu position="bottom-start">
+      <Menu.Target>
+        <ActionIcon variant="light" color="gray" aria-label="ナビメニュー">
+          {currentMenu?.icon ?? <IconCaretDownFilled size={14} />}
+        </ActionIcon>
+      </Menu.Target>
+      <Menu.Dropdown>
+        {Object.entries(MenuMap).map(([, { pathname, icon, title }]) => (
+          <Menu.Item key={title} component={Link} leftSection={icon} href={pathname}>
+            {title}
+          </Menu.Item>
+        ))}
+      </Menu.Dropdown>
+    </Menu>
+  );
+};
+
+export default NavMenu;
