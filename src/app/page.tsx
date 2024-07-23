d257d179dc73d2ca0dc51997172c43f01d149ec5
RyoSogawa <koashimitekara.1122@gmail.com>
2024-07-23 22:20:26 +0900

    feat:minimal top page

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Container, Title } from '@mantine/core';
+import Link from 'next/link';
+
+export default async function Home() {
+  return (
+    <Container py={16}>
+      <Title>Home</Title>
+      <ul>
+        <li>
+          <Link href="/members">部員名簿</Link>
+        </li>
+      </ul>
+    </Container>
+  );
+}
