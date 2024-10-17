35892d864c82006aec55fd7a69325223e3122d6d
RyoSogawa <koashimitekara.1122@gmail.com>
2024-10-17 17:41:19 +0900

    fix:build error(t3app bug)

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,9 +1,17 @@
 import { Container, SimpleGrid, Title } from '@mantine/core';
+import { unstable_noStore } from 'next/cache';
 
 import EventCard from '@/features/event/components/EventCard';
 import { api } from '@/trpc/server';
 
 export default async function Events() {
+  /**
+   * HACK: t3-qppのバグ対応
+   * [TRPCClientError]: Dynamic server usage: Page couldn't be rendered statically because it used `headers`.
+   * @see https://github.com/t3-oss/create-t3-app/issues/1599#issuecomment-2007564869
+   */
+  unstable_noStore();
+
   const [events] = await Promise.all([api.event.getAll.query()]);
 
   return (
